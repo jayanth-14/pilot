1a4ebc150064451bd401c27d4736161f1461b3dc
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-21 17:56:22 +0530

    implemented uninstalling pilot

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-# Uninstalls pilot from the host by deleting the binary executable from 'usr/local/bin/pilot';
+# Uninstalls pilot from the host by deleting the binary executable from '/usr/local/bin/pilot';
+
+if [ -e /usr/local/bin/pilot ]; then
+  rm /usr/local/bin/pilot
+  echo 'pilot deleted ✅';
+else
+  echo 'pilot is not available';
+fi;
+
