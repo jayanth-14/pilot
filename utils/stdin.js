828d8afea5991edfc31df2c26356992e436f2b4b
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-23 05:33:36 +0530

    implemented check for interactive mode

--- utils/stdin.js
+++ utils/stdin.js
@@ -3,3 +3,11 @@ Reads all data from stdin (piped data, redirections, etc.)
 Returns an empty string if no data is available.
 */
 
+export const readStdin = async () => {
+  // Implementing early exit in case its in interactive mode.
+  if (Deno.stdin.isTerminal()) return '';
+
+  return 'Not in interactive mode';
+}
+
+console.log(await readStdin());
