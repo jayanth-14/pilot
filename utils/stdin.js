2574ac662dfe8859404ab803e33afe0c5979510a
jayanth-14 <jayanthpetchetii@gmail.com>
2025-12-23 05:46:51 +0530

    implemented function to read input from stdin

--- utils/stdin.js
+++ utils/stdin.js
@@ -7,7 +7,16 @@ export const readStdin = async () => {
   // Implementing early exit in case its in interactive mode.
   if (Deno.stdin.isTerminal()) return '';
 
-  return 'Not in interactive mode';
+  const decoder = new TextDecoder();
+  let input = '';
+  
+  // decoding each chunk from the input stream
+  for await(const chunk of Deno.stdin.readable) {
+    input += decoder.decode(chunk, { stream : true });
+  }
+  // flushing any reamaing bytes
+  input += decoder.decode();
+
+  return input;
 }
 
-console.log(await readStdin());
